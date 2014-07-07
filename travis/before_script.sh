2edbc2de0934605ced2317dfe6fed5a618eade64
Peter Wilemsen <peter@codebuffet.co>
2014-07-07 20:08:54 +0200

    Unity installer via command line... Fingers crossed..

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -5,7 +5,7 @@ wget http://netstorage.unity3d.com/unity/unity-4.5.1.dmg
 sudo hdiutil attach unity-4.5.1.dmg
 
 unity_installer_path='/Volumes/Unity Installer'
-
-ls "$unity_installer_path"
+cd /
+sudo /usr/sbin/installer -pkg "$unity_installer_path/Unity.pkg" -target /
 
 exit 0
