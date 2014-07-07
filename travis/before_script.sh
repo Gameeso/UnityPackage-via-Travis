2d801e39a6fb2148c411f39289df8b0eaadcb584
Peter Wilemsen <peter@codebuffet.co>
2014-07-07 19:31:48 +0200

    Ok download & mount works, now check the folder contents if there is a .pkg file, figuring that out later

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -3,6 +3,9 @@
 echo "Downloading Unity Editor"
 wget http://netstorage.unity3d.com/unity/unity-4.5.1.dmg
 sudo hdiutil attach unity-4.5.1.dmg
-ls /Volumes
+
+unity_installer_path='/Volumes/Unity Installer'
+
+ls "$unity_installer_path"
 
 exit 0
